eccb0154255873259a8b357853bdba954e8865f8
Unknown6656 <unknown84@gmx.de>
2017-09-01 13:16:08 +0200

    Initial code commit

---
+++ LXFML.cs
@@ -0,0 +1,213 @@
+﻿using System.Xml.Serialization;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Xml.Schema;
+using System;
+
+namespace LXFPartListCreator
+{
+    using static XmlSchemaForm;
+
+
+    // AUTO-GENERATED from 'lxfml.xsd'
+
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true), XmlRoot(Namespace = "", IsNullable = false)]
+    public partial class Camera
+    {
+        [XmlAttribute]
+        public string refID { set; get; }
+        [XmlAttribute]
+        public string fieldOfView { set; get; }
+        [XmlAttribute]
+        public string distance { set; get; }
+        [XmlAttribute]
+        public string transformation { set; get; }
+        [XmlAttribute]
+        public string cameraRef { set; get; }
+    }
+    
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true), XmlRoot(Namespace = "", IsNullable = false)]
+    public partial class Step
+    {
+        [XmlElement("PartRef", Form = Unqualified)]
+        public StepPartRef[] PartRef { set; get; }
+        [XmlElement("Camera")]
+        public Camera[] Camera { set; get; }
+        [XmlElement("Step")]
+        public Step[] Step1 { set; get; }
+        [XmlAttribute]
+        public string name { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class StepPartRef
+    {
+        [XmlAttribute]
+        public string partRef { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true), XmlRoot(Namespace = "", IsNullable = false)]
+    public partial class LXFML
+    {
+        [XmlElement("Meta", Form = Unqualified)]
+        public LXFMLMeta[] Meta { set; get; }
+        [XmlArray(Form = Unqualified), XmlArrayItem("Camera", typeof(Camera), IsNullable = false)]
+        public Camera[] Cameras { set; get; }
+        [XmlElement("Bricks", Form =Unqualified)]
+        public LXFMLBricks[] Bricks { set; get; }
+        [XmlArray(Form = Unqualified), XmlArrayItem("RigidSystem", typeof(LXFMLRigidSystemsRigidSystem), Form = Unqualified, IsNullable = false)]
+        public LXFMLRigidSystemsRigidSystem[] RigidSystems { set; get; }
+        [XmlElement("GroupSystems", Form = Unqualified)]
+        public LXFMLGroupSystems[] GroupSystems { set; get; }
+        [XmlArray(Form = Unqualified), XmlArrayItem("BuildingInstruction", typeof(LXFMLBuildingInstructionsBuildingInstruction), Form = Unqualified, IsNullable = false)]
+        public LXFMLBuildingInstructionsBuildingInstruction[] BuildingInstructions { set; get; }
+        [XmlAttribute]
+        public string versionMajor { set; get; }
+        [XmlAttribute]
+        public string versionMinor { set; get; }
+        [XmlAttribute]
+        public string name { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLMeta
+    {
+        [XmlElement("Application", Form = Unqualified)]
+        public LXFMLMetaApplication[] Application { set; get; }
+        [XmlElement("Brand", Form = Unqualified)]
+        public LXFMLMetaBrand[] Brand { set; get; }
+        [XmlElement("BrickSet", Form = Unqualified)]
+        public LXFMLMetaBrickSet[] BrickSet { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLMetaApplication
+    {
+        [XmlAttribute]
+        public string name { set; get; }
+        [XmlAttribute]
+        public string versionMajor { set; get; }
+        [XmlAttribute]
+        public string versionMinor { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLMetaBrand
+    {
+        [XmlAttribute]
+        public string name { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLMetaBrickSet
+    {
+        [XmlAttribute]
+        public string version { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLBricks
+    {
+        [XmlElement("Brick", Form = Unqualified)]
+        public LXFMLBricksBrick[] Brick { set; get; }
+        [XmlAttribute]
+        public string cameraRef { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLBricksBrick
+    {
+        [XmlElement("Part", Form = Unqualified)]
+        public LXFMLBricksBrickPart[] Part { set; get; }
+        [XmlAttribute]
+        public string refID { set; get; }
+        [XmlAttribute]
+        public string designID { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLBricksBrickPart
+    {
+        [XmlElement("Bone", Form = Unqualified)]
+        public LXFMLBricksBrickPartBone[] Bone { set; get; }
+        [XmlAttribute]
+        public string refID { set; get; }
+        [XmlAttribute]
+        public string designID { set; get; }
+        [XmlAttribute]
+        public string materials { set; get; }
+        [XmlAttribute]
+        public string decoration { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLBricksBrickPartBone
+    {
+        [XmlAttribute]
+        public string refID { set; get; }
+        [XmlAttribute]
+        public string transformation { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLRigidSystemsRigidSystem
+    {
+        [XmlElement("Rigid", Form = Unqualified)]
+        public LXFMLRigidSystemsRigidSystemRigid[] Rigid { set; get; }
+        [XmlElement("Joint", Form = Unqualified)]
+        public LXFMLRigidSystemsRigidSystemJoint[] Joint { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLRigidSystemsRigidSystemRigid
+    {
+        [XmlAttribute]
+        public string refID { set; get; }
+        [XmlAttribute]
+        public string transformation { set; get; }
+        [XmlAttribute]
+        public string boneRefs { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLRigidSystemsRigidSystemJoint
+    {
+        [XmlElement("RigidRef", Form = Unqualified)]
+        public LXFMLRigidSystemsRigidSystemJointRigidRef[] RigidRef { set; get; }
+        [XmlAttribute]
+        public string type { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLRigidSystemsRigidSystemJointRigidRef
+    {
+        [XmlAttribute]
+        public string rigidRef { set; get; }
+        [XmlAttribute]
+        public string a { set; get; }
+        [XmlAttribute]
+        public string z { set; get; }
+        [XmlAttribute]
+        public string t { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLGroupSystems
+    {
+        [XmlElement(Form = Unqualified)]
+        public string GroupSystem { set; get; }
+    }
+
+    [GeneratedCode("xsd", "4.6.1055.0"), Serializable, DebuggerStepThrough, DesignerCategory("code"), XmlType(AnonymousType = true)]
+    public partial class LXFMLBuildingInstructionsBuildingInstruction
+    {
+        [XmlElement("Camera")]
+        public Camera[] Camera { set; get; }
+        [XmlElement("Step")]
+        public Step[] Step { set; get; }
+        [XmlAttribute]
+        public string name { set; get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System;
+
+namespace LXFPartListCreator
+{
+    using static ConsoleColor;
+    using static Console;
+
+    public static class Program
+    {
+        public const string PATH_LXFML = "IMAGE100.LXFML";
+        public const string PATH_PNG = "IMAGE100.PNG";
+
+
+        public static void Main(string[] argv)
+        {
+            Directory.SetCurrentDirectory(new FileInfo(typeof(Program).Assembly.Location).Directory.FullName);
+
+            Dictionary<string, string> opt = GetOptions(argv);
+            bool hasopt(string n, out string a)
+            {
+                (a, n) = (null, n.ToLower());
+
+                if (opt.ContainsKey(n))
+                    a = opt[n];
+
+                return a != null;
+            }
+            
+            if (hasopt("in", out string @in) && hasopt("out", out string @out))
+                try
+                {
+                    string thumbnail;
+                    LXFML lxfml;
+
+                    using (ZipArchive lxf = ZipFile.Open(@in, ZipArchiveMode.Read))
+                    using (Stream lxfms = lxf.GetEntry(PATH_LXFML).Open())
+                    using (Stream pngms = lxf.GetEntry(PATH_PNG).Open())
+                    using (Bitmap png = Image.FromStream(pngms) as Bitmap)
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        XmlSerializer xmlser = new XmlSerializer(typeof(LXFML));
+
+                        lxfml = xmlser.Deserialize(lxfms) as LXFML;
+
+                        png.Save(ms, ImageFormat.Png);
+
+                        thumbnail = $"data:image/png;base64,{Convert.ToBase64String(ms.ToArray())}";
+                    }
+
+                    FileInfo nfo = new FileInfo(@out);
+
+                    if (nfo.Exists)
+                        nfo.Delete();
+
+                    using (FileStream fs = nfo.OpenWrite())
+                    using (StreamWriter wr = new StreamWriter(fs, Encoding.UTF8))
+                        wr.Write(GenerateHTML(lxfml, thumbnail));
+
+                    if (hasopt("open-after-success", out _))
+                        Process.Start(@out);
+                }
+                catch (Exception ex)
+                {
+                    ex.Err();
+                }
+            else
+                "No input and/or output file path has been provided.".Err();
+
+            if (Debugger.IsAttached)
+            {
+                WriteLine("Press any key to exit ...");
+                ReadKey(true);
+            }
+        }
+
+        private static Dictionary<string, string> GetOptions(string[] argv)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string> {
+                [""] = "",
+            };
+
+            foreach (string s in argv ?? new string[0])
+                if (!string.IsNullOrWhiteSpace(s))
+                {
+                    if (s.match(@"(\-\-(?<name>[\w\-]+)(\=(?<val>.*))?|\-(?<name>[\w\-]+)(\=(?<val>.*))?|\/(?<name>[\w\-]+)(\:(?<val>.*))?)", out Match m))
+                        dic[m.Groups["name"].ToString().ToLower()] = m.Groups["val"].ToString();
+                    else
+                        dic[""] += ' ' + s;
+                }
+
+            return dic;
+        }
+
+        private static string DFormat(this string formatstring, Dictionary<string, string> dic)
+        {
+            Dictionary<string, int> kti = new Dictionary<string, int>();
+            StringBuilder sb = new StringBuilder(formatstring);
+            int i = 0;
+
+            sb = sb.Replace("{", "{{")
+                   .Replace("}", "}}");
+
+            foreach (var tuple in dic)
+            {
+                sb = sb.Replace("§" + tuple.Key + "§", "{" + i.ToString() + "}");
+
+                kti.Add(tuple.Key, i);
+
+                ++i;
+            }
+
+            sb = sb.Replace("§§", "§");
+
+            return String.Format(sb.ToString(), dic.OrderBy(x => kti[x.Key]).Select(x => x.Value).ToArray());
+        }
+
+        private static string GenerateHTML(LXFML lxfml, string thumbnail)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool match(this string s, string pat, out Match m, RegexOptions opt = RegexOptions.Compiled | RegexOptions.IgnoreCase) =>
+            (m = Regex.Match(s, pat, opt)).Success;
+
+        private static void Err(this string str)
+        {
+            ConsoleColor clr = ForegroundColor;
+
+            ForegroundColor = Red;
+            WriteLine(str);
+            ForegroundColor = clr;
+        }
+
+        private static void Err(this Exception err)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (err != null)
+            {
+                sb.AppendLine(err.Message);
+                sb.AppendLine(err.StackTrace);
+
+                err = err.InnerException;
+            }
+
+            sb.ToString().Err();
+        }
+    }
+}
